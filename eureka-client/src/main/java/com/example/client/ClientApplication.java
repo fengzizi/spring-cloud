78bdb4c405f8c6ccf340c523f4158a4ee6bada81
峰子 <854214434@qq.com>
2019-08-09 18:13:30 +0800

    ``

--- eureka-client/src/main/java/com/example/client/ClientApplication.java
+++ eureka-client/src/main/java/com/example/client/ClientApplication.java
@@ -2,10 +2,19 @@ package com.example.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
 @EnableEurekaClient
+
+@EnableHystrix	//这个是必须的，并且需要在程序中声明断路点HystrixCommand
+@EnableHystrixDashboard	//加上@EnableHystrixDashboard注解，开启HystrixDashboard
+@EnableCircuitBreaker
+@EnableDiscoveryClient
 public class ClientApplication {
 
 	public static void main(String[] args) {
--- eureka-client/src/main/java/com/example/client/controller/TestController.java
+++ eureka-client/src/main/java/com/example/client/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.client.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,12 @@ public class TestController {
     private String port;
 
     @RequestMapping("/hi")
+    @HystrixCommand(fallbackMethod = "hiError")
     public String home(String name) {
         return "hi " + name + " ,i am from port:" + port;
+    }
+
+    public String hiError(String name) {
+        return "hi,"+name+",sorry,error!";
     }
 }
